0bb490827e8853d4752d1bf1374ad7ff61632e04
a1ef0 <a.buchnev@innopolis.university>
2021-08-04 15:38:46 +0300

    solved with set

--- lab3/asd.cpp
+++ lab3/asd.cpp
@@ -4,7 +4,26 @@ using namespace std;
 
 int main(){
 
-    
+    int n, t;
+    cin >> n;
+    if (n < 1) {
+        cout << "no\n";
+        return 0;
+    }
 
+    vector<int> a(n);
+    set<int> se;
+
+    for (int i = 0; i < n; ++i){
+        cin >> t;
+        a[i] = t;
+        se.insert(t);
+    }
+
+
+    if (se.size() != n){
+        cout << "the array is not unique!\n";
+    }
+    else cout << "unique\n";
     return 0;
 }
